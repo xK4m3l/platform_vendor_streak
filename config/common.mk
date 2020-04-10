2ec3c592c8c02502244a105d4085c3aa6afc0917
Ido Ben-Hur <idoybh2@gmail.com>
2020-04-10 18:06:19 +0300

    vendor: Build AOSP SettingsIntelligence

--- config/common.mk
+++ config/common.mk
@@ -21,6 +21,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html
 
+# SI
+PRODUCT_PACKAGES += \
+	SettingsIntelligence
+
 ifeq ($(DERP_BUILD_ZIP_TYPE), VANILLA)
 PRODUCT_PACKAGES += \
     libjni_latinimegoogle
