79ab125b0ccda24bf3a233bcae7a5cb467255923
TheScarastic <warabhishek@gmail.com>
2020-08-17 17:21:02 +0000

    soong: Add flag for fod extension
    Change-Id: Ib45706cc50fa88b9b05af1c68eec9d1f08d70569

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -37,6 +37,10 @@ type Product_variables struct {
 	Target_shim_libs struct {
 		Cppflags []string
 	}
+	Target_surfaceflinger_fod_lib struct {
+		Cflags []string
+		Whole_static_libs []string
+	}
 	Target_needs_netd_direct_connect_rule struct {
 		Cppflags []string
 	}
@@ -71,6 +75,7 @@ type ProductVariables struct {
 	Target_init_vendor_lib  *string `json:",omitempty"`
 	Target_process_sdk_version_override  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
+	Target_surfaceflinger_fod_lib  *string `json:",omitempty"`
 	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_metadata_as_fde_key  *bool `json:",omitempty"`
