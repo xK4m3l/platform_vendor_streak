AOSPA/android_vendor_pa

config: Bring back Android Beam
* While this feature is deprecated all code
  for it is still alive ^.^

Change-Id: I36fac374e43ceddab1438255654bb541937041ff

config/common.mk
@@ -23,6 +23,10 @@ PRODUCT_PACKAGES += \
     adb_root
 endif
 
+# Android Beam
+PRODUCT_COPY_FILES += \
+    vendor/pa/config/permissions/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.nfc.beam.xml
+
 # ART
 # Optimize everything for preopt
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything

xK4m3l/platform_vendor_streak

config/common.mk
@@ -75,6 +75,10 @@ PRODUCT_COPY_FILES += \
     vendor/streak/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-elgoog.xml \
     vendor/streak/prebuilt/common/etc/permissions/privapp-permissions-streak.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-streak.xml
 
+# Android Beam
+PRODUCT_COPY_FILES += \
+    vendor/streak/config/permissions/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.nfc.beam.xml
+
 # Set custom volume steps
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.config.media_vol_steps=30 \