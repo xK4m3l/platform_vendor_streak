649b49dc0e0e3a9bdc67eda582f5c6e8116d0794
HSgod <hsgodkontakt@gmail.com>
2020-09-05 20:59:48 +0000

    Revert "vendor: Add lawnchair"
    This reverts commit 388fe441026d85170ecfadccd32ab6e1fb807507.

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -1,17 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := Lawnchair
-LOCAL_SRC_FILES := priv-app/$(LOCAL_MODULE).apk
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3 Launcher3QuickStep
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_DEX_PREOPT := false
-LOCAL_REPLACE_PREBUILT_APK_INSTALLED := $(LOCAL_PATH)/$(LOCAL_SRC_FILES)
-include $(BUILD_PREBUILT)
-
 ifeq ($(TARGET_USES_AOSP_APNS_CONF),)
 ################################
 # Copies the APN list file into $(TARGET_COPY_OUT_PRODUCT)/etc for the product as apns-conf.xml.
