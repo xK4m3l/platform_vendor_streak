d2a457f648adfc2e2f578e0737736ee8a931d861
HSgod <hsgodkontakt@gmail.com>
2020-10-07 16:05:03 +0000

    vendor: Add apn includes in common
    Signed-off-by: HSgod <hsgodkontakt@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -123,5 +123,25 @@ PRODUCT_PACKAGES += \
     PixelSetupWizardOverlay \
     PixelSetupWizardAodOverlay
 
+# World APN list
+PRODUCT_PACKAGES += \
+    apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/streak/prebuilt/common/etc/spn-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/spn-conf.xml
+
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/streak/prebuilt/common/etc/selective-spn-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/selective-spn-conf.xml
+
+# SIM Toolkit
+PRODUCT_PACKAGES += \
+    Stk
+
+# Sensitive Phone Numbers list
+PRODUCT_COPY_FILES += \
+    vendor/streak/prebuilt/common/etc/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml
+
 # include definitions for SDCLANG
 include vendor/streak/sdclang/sdclang.mk
