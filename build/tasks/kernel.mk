09164bbe758c7d23372508f4984588275e00cfd9
HSgod <hsgodkontakt@gmail.com>
2020-10-09 14:53:55 +0000

    vendor: Fix perl path
    Signed-off-by: HSgod <hsgodkontakt@gmail.com>

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -192,7 +192,7 @@ ifeq ($(TARGET_KERNEL_CLANG_COMPILE),true)
     endif
     TARGET_KERNEL_CLANG_PATH ?= $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/$(KERNEL_CLANG_VERSION)
     KBUILD_COMPILER_STRING := $(shell $(TARGET_KERNEL_CLANG_PATH)/bin/clang --version | head -n 1 | \
-	    $(BUILD_TOP)/prebuilts/tools-lineage/$(HOST_OS)-x86/bin/perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')
+	    $(BUILD_TOP)/prebuilts/tools-streak/$(HOST_OS)-x86/bin/perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')
     KERNEL_MAKE_FLAGS += KBUILD_COMPILER_STRING="$(KBUILD_COMPILER_STRING)"
     ifeq ($(KERNEL_ARCH),arm64)
         KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=aarch64-linux-gnu-
