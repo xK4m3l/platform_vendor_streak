Octavi-OS/platform_vendor_octavi

PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS

config/common.mk
@@ -111,3 +111,6 @@ include packages/overlays/Themes/themes.mk
 
 # Branding
 include vendor/octavi/config/branding.mk
+
+# Overlays
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/octavi/overlay

xK4m3l/platform_vendor_streak

config/common.mk
@@ -148,3 +148,6 @@ include vendor/streak/sdclang/sdclang.mk
 
 # Fonts
 include vendor/streak/config/fonts.mk
+
+# Overlays
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/streak/overlay